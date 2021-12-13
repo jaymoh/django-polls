4250416c3efdedd04fafbf2b9cba30a42141e684
James Shisiah <jamesshisiah@gmail.com>
2021-12-13 22:30:09 +0300

    Adds Dockerfile and environment variables configurations

--- django-polls/mysite/settings.py
+++ django-polls/mysite/settings.py
@@ -9,24 +9,24 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import json
+import os
 from pathlib import Path
+import logging.config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your_secret_key'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 # Application definition
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'polls.apps.PollsConfig'
 ]
 
@@ -70,20 +71,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'polls_db',
-        'USER': 'shisiah',
-        'PASSWORD': '0701437909',
-        'HOST': '127.0.0.1'
+        'ENGINE': 'django.db.backends.{}'.format(
+            os.getenv('DB_ENGINE', 'sqlite3')
+        ),
+        'NAME': os.getenv('DB_DATABASE', 'django_polls'),
+        'USER': os.getenv('DB_USERNAME', 'your_db_username'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'your_secure_default_password'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', 5432),
+        'OPTIONS': json.loads(
+            os.getenv('DB_OPTIONS', '{}')
+        ),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -103,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -117,13 +121,59 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+GS_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+GS_BUCKET_NAME = os.getenv('STATIC_GS_BUCKET_NAME')
+
+DEFAULT_FILE_STORAGE = os.getenv('STATIC_DEFAULT_FILE_STORAGE', 'storages.backends.gcloud.GoogleCloudStorage')
+GS_ENDPOINT_URL = os.getenv('STATIC_ENDPOINT_URL')
+
+GS_OBJECT_PARAMETERS = {
+    'cache_control': 'max-age=86400',
+}
+
+GS_LOCATION = 'static'
+GS_DEFAULT_ACL = 'publicRead'
+
+STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+
+STATIC_URL = '{}/{}/'.format(GS_ENDPOINT_URL, GS_LOCATION)
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logging Configuration
+
+# Disable prev config
+LOGGING_CONFIG = None
+
+# Get loglevel from env
+LOGLEVEL = os.getenv('DJANGO_LOGLEVEL', 'info').upper()
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'loggers': {
+        '': {
+            'level': LOGLEVEL,
+            'handlers': ['console',],
+        },
+    },
+})
